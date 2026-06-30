c5f27cb5db7713c8fb8476db4bb25eccf35e2ff3
Joe <Joe@DESKTOP-9L0VJF1>
2026-06-30 23:41:39 +0300

    add addons controller

---
+++ KDS.Server/Controllers/AddOnsController.cs
@@ -0,0 +1,33 @@
+﻿using KDS.Server.DTOs;
+using KDS.Server.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KDS.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class AddOnsController : ControllerBase
+    {
+        private readonly IAddOnsService _svc;
+        public AddOnsController(IAddOnsService svc)
+        {
+            _svc = svc;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AddOnDto>>> GetAll([FromQuery] bool activeOnly = false)
+        => Ok(await _svc.GetAllAsync(activeOnly));
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult<AddOnDto>> Create(CreateAddOnDto dto)
+        => Ok(await _svc.CreateAsync(dto));
+
+        [Authorize(Roles = "Admin")]
+        [HttpPatch("{id:int}/toggle-active")]
+        public async Task<IActionResult> Toggle(int id)
+        => await _svc.ToggleActiveAsync(id) ? Ok() : NotFound();
+    }
+}
