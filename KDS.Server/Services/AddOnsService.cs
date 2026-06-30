c78243bf52f7b196920addd3c9b0c5efa4048f4d
Joe <Joe@DESKTOP-9L0VJF1>
2026-06-30 23:32:37 +0300

    added addon service

---
+++ KDS.Server/Services/AddOnsService.cs
@@ -0,0 +1,61 @@
+﻿using KDS.Server.Data;
+using KDS.Server.DTOs;
+using KDS.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KDS.Server.Services
+{
+    public class AddOnsService : IAddOnsService
+    {
+        private readonly AppDbContext _dbContext;
+        public AddOnsService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<AddOnDto>> GetAllAsync(bool activeOnly)
+        {
+            var query = _dbContext.AddOns.AsQueryable();
+            if (activeOnly) query = query.Where(a => a.IsActive);
+
+            return await query.OrderBy(a => a.Name).Select(a => new AddOnDto
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Price = a.Price,
+                IsRemoval = a.IsRemoval,
+                IsActive = a.IsActive
+            }).ToListAsync();
+        }
+
+        public async Task<AddOnDto> CreateAsync(CreateAddOnDto dto)
+        {
+            var a = new AddOn
+            {
+                Name = dto.Name,
+                Price = dto.Price,
+                IsRemoval = dto.IsRemoval,
+                IsActive = dto.IsActive
+            };
+            _dbContext.AddOns.Add(a);
+            await _dbContext.SaveChangesAsync();
+            return new AddOnDto
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Price = a.Price,
+                IsRemoval = a.IsRemoval,
+                IsActive = a.IsActive
+            };
+        }
+
+        public async Task<bool> ToggleActiveAsync(int id)
+        {
+            var a = await _dbContext.AddOns.FindAsync(id);
+            if (a is null) return false;
+            a.IsActive = !a.IsActive;
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ KDS.Server/Services/IAddOnsService.cs
@@ -0,0 +1,11 @@
+﻿using KDS.Server.DTOs;
+
+namespace KDS.Server.Services
+{
+    public interface IAddOnsService
+    {
+        Task<IEnumerable<AddOnDto>> GetAllAsync(bool activeOnly);
+        Task<AddOnDto> CreateAsync(CreateAddOnDto dto);
+        Task<bool> ToggleActiveAsync(int id);
+    }
+}
