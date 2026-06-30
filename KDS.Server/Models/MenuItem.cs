6ea4749b15bc24ed1b9c5faaad36f07821186b40
Joe <Joe@DESKTOP-9L0VJF1>
2026-06-30 23:30:19 +0300

    add menuItem, menuItemComponent models

---
+++ KDS.Server/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿namespace KDS.Server.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal BasePrice { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public int PrepTimeMinutes { get; set; } = 10;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public ICollection<MenuItemComponent> Components { get; set; } = new List<MenuItemComponent>();
+    }
+}
+
---
+++ KDS.Server/Models/MenuItemComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace KDS.Server.Models
+{
+    public class MenuItemComponent
+    {
+        public int Id { get; set; }
+        public int MenuItemId { get; set; }
+        public int ComponentId { get; set; }
+        public int Quantity { get; set; } = 1;   // multiplier: 4 for Box of 4
+
+        public MenuItem? MenuItem { get; set; }
+        public Component? Component { get; set; }
+    }
+}
