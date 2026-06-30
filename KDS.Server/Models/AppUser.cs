b7e21aadc67427e41356f0723591e3949511de8f
Joe <Joe@DESKTOP-9L0VJF1>
2026-06-30 23:28:54 +0300

    added appUser model

---
+++ KDS.Server/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace KDS.Server.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int? StationId { get; set; }
+        public Station? Station { get; set; }
+    }
+}
